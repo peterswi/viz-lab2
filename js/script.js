f0d50b075791ec94c7f98b8e6ca64742dce085c0
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-09-13 16:20:54 -0400

    Making progress

--- js/script.js
+++ js/script.js
@@ -1,33 +1,44 @@
 
-let attractions;
-fetch('attractions.json')
-    .then(response => response.json())
-    .then(data => {
-        attractions=data;
-        console.log(attractions);
-    }
-    )
-
-let topAttractions = attractions.sort((a,b)=> a.Visitors-b.Visitors);
-console.log(topAttractions)
+ let attractions;
+ let topFive;
+ fetch('attractions.json')
+     .then(response => response.json())
+     .then(data => {
+         attractions=data;
+         attractions.sort((a,b)=> b.Visitors-a.Visitors);
+         console.log('sorted',attractions)
+         topFive=attractions.slice(0,5);
+         console.log('Top five by visitors:',topFive);
+         renderBarChart(topFive);
+     }
+ )   
+ 
+// let topAttractions = attractions.sort((a,b)=> a.Visitors-b.Visitors);
+// console.log(topAttractions)
 
 function filterData(category) {
-
-    /* **************************************************
-     *
-     * TODO: filter attractions by the selected category
-     * TODO: filter top 5 attractions
-     *
-     * CALL THE FOLLOWING FUNCTION TO RENDER THE BAR-CHART:
-     *
-     * renderBarChart(data)
-     *
-     * - 'data' must be an array of JSON objects
-     * - the max. length of 'data' is 5
-     *
-     * **************************************************/
-
+    let attractions;
+    let topFive;
+    fetch('attractions.json')
+        .then(response => response.json())
+        .then(data => {
+            attractions=data;
+            // this isnt correct! attractions.filter(category)
+            attractions.sort((a,b)=> b.Visitors-a.Visitors);
+            console.log('sorted',attractions)
+            topFive=attractions.slice(0,5);
+            console.log('Top five by visitors:',topFive);
+            renderBarChart(topFive);
+        }
+    )   
+    
+   
 }
+
 
 // TODO: Define an event listener for the dropdown menu
 //       Call filterData with the selected category
+function onchange(e) {
+    filterData(e.target.value)
+}
+document.querySelector('#attraction-category').addEventListener('change',onchange)
